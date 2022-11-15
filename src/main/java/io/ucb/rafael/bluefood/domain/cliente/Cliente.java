72e84635f931bd7dff238fd4f7ccba41447580a1
Rafael Riki Ogawa Osiro <rafaelosiro@hotmail.com>
2022-11-15 15:20:12 -0300

    Feat: Created User Entity

---
+++ src/main/java/io/ucb/rafael/bluefood/domain/cliente/Cliente.java
@@ -0,0 +1,20 @@
+package io.ucb.rafael.bluefood.domain.cliente;
+
+import javax.persistence.Entity;
+
+import io.ucb.rafael.bluefood.domain.usuario.Usuario;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@SuppressWarnings("serial")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
+@Entity
+public class Cliente extends Usuario {
+
+	private String cpf;
+	private String cep;
+
+}
